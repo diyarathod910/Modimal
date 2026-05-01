b2a44b4187117ead6ee56b7dfc17f7a024239b69
diyarathod910 <diyarathod910@gmail.com>
2026-05-01 12:13:05 +0530

    modifed in search/page.js and added new file

--- frontend/src/app/search/page.js
+++ frontend/src/app/search/page.js
@@ -1,58 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import SearchPage from "./searchPage";
 
-import { useSearchParams } from "next/navigation";
-import {useState } from "react";
-
-import FilterSidebar from "../components/shop/filterSidebar";
-import useWishlist from "../hooks/useWishlist";
-import SearchGrid from "../components/navbar/searchGrid";
-
-export default function SearchPage() {
-    const searchParams = useSearchParams();
-    const query = searchParams.get("q");
-
-   
-
-    const { wishlist, toggleWishlist, isInWishlist } = useWishlist();
-
-    const sizeOptions = ["s", "m", "l"];
-
-    const [filters, setFilters] = useState({
-        size: [],
-        color: [],
-        fabric: [],
-        inStock: false,
-        sort: ""
-    });
-
-
-    
-
+export default function Page() {
     return (
-        <div className="w-full px-20 py-10">
-
-            {/* 🔹 Title */}
-            <h1 className="text-2xl mb-6">
-                Search results for "<span className="font-semibold">{query}</span>"
-            </h1>
-
-         
-
-            {/* ✅ Product Grid */}
-            <div className="flex justify-center items-center">
-                <div className="w-[85%] flex  justify-between mt-[5%]" >
-                    <div className="w-[25%]">
-                        {/* <FilterSidebar></FilterSidebar> */}
-                        <FilterSidebar filters={filters} setFilters={setFilters} />
-                    </div>
-                    <div className="w-[70%]">
-                        {/* <ProductGrid sizeOptions={sizeOptions}></ProductGrid> */}
-                        <SearchGrid sizeOptions={sizeOptions} filters={filters} wishlist={wishlist}
-                            toggleWishlist={toggleWishlist}
-                            isInWishlist={isInWishlist} />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <SearchPage />
+        </Suspense>
     );
 }
