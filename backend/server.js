c3835e28e18adeefdbab7466c958e5d07cc8325c
diyarathod910 <diyarathod910@gmail.com>
2026-05-04 10:05:28 +0530

    changes in sever.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const path = require("path");
+import { fileURLToPath } from "url";
 // const cookieValidator = require('./middleware/auth.js')
 
 
@@ -44,6 +45,9 @@ async function validateCookies(req, res, next) {
 }
 
 // app.use("/uploads", express.static("uploads"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(bodyParser.json());
